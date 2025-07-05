735f7bc60ca28aad1a3ed35b3fe488c080a33037
Mahmoud0980 <mahmoud98hh@gmail.com>
2025-07-05 10:13:38 +0300

    Fix some problem

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
     { href: "https://www.google.com", label: "تواصل معنا" },
   ];
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary mt-3 rounded-3 shadow-sm   ">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary rounded-3 shadow mb-4 mt-2">
       <div className="container-fluid">
         <a className="navbar-brand d-flex align-items-center gap-2" href="#">
           <Image
